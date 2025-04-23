80bedfaf5999134f08049915e43bc5bfcbfc1907
Kris <kris.kajtna@gmail.com>
2025-04-23 21:07:31 +0200

    feat: add full-width search functionality to PageHeader component

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,11 +1,13 @@
-import { Bell, Menu, Mic, Search, Upload, User } from 'lucide-react';
+import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from 'lucide-react';
 import logo from '../assets/logo3.png';
 import { Button } from '../components/Button';
+import { useState } from 'react';
 
 export function PageHeader() {
+    const [showFullWidthSearch, setShowFullWidthSearch] = useState(false) 
   return (
     <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-        <div className="flex gap-4 items-center flex-shrink-0">
+        <div className={`gap-4 items-center flex-shrink-0 ${showFullWidthSearch ? 'hidden' : 'flex'}`}>
             <Button variant="ghost" size="icon" >
                 <Menu />
             </Button>
@@ -13,12 +15,21 @@ export function PageHeader() {
                 <img src={logo} className="h-6" />
             </a>
         </div>
-        <form className="flex gap-4 flex-grow justify-center">
+        <form className={` gap-4 flex-grow justify-center 
+            ${showFullWidthSearch ? "flex" : "hidden md:flex"}`}>
+                {showFullWidthSearch && (
+                <Button onClick={() => setShowFullWidthSearch(false)} type='button' size="icon" variant="ghost" className='flex-shrink-0'>
+                    <ArrowLeft />
+                </Button>
+            )}
             <div className="flex flex-grow max-w-[600px]">
-                <input type='search' placeholder='Search' 
-                className="rounded-1-full border border-secondary-border shadow-inner 
-                shadow-secondary"/>
-                <Button>
+                <input 
+                type='search' 
+                placeholder='Search' 
+                className="rounded-l-full border border-secondary-border shadow-inner 
+                shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"/>
+                <Button className='py-2 px-4 rounded-r-full border-secondary-border border 
+                border-l-0 flex-shrink-0'>
                     <Search />
                 </Button>
             </div>
@@ -26,7 +37,13 @@ export function PageHeader() {
                 <Mic />
             </Button>
         </form>
-        <div className="flex flex-shrink-0 md:gap-2 ">
+        <div className={`flex-shrink-0 md:gap-2 ${showFullWidthSearch ? 'hidden' : 'flex'} `}>
+            <Button onClick={() => setShowFullWidthSearch(true)} variant="ghost" size="icon" className='md:hidden'>
+                <Search />
+            </Button>
+            <Button variant="ghost" size="icon" className='md:hidden'>
+                <Mic />
+            </Button>
             <Button variant="ghost" size="icon">
                 <Upload />
             </Button>
