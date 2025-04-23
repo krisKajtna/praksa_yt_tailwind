ca5046e3ec39693e58b1194f07047113c6e184ef
Kris <kris.kajtna@gmail.com>
2025-04-23 19:57:02 +0200

    feat: add Button component and update PageHeader to use it

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,13 +1,16 @@
 import { Menu } from 'lucide-react';
 import logo from '../assets/logo3.png';
+import { Button } from '../components/Button';
 
 export function PageHeader() {
   return (
-    <div className="flex gap-10 lg:gap-20 justify-between">
-        <div className="flex gap-4 items-center flex-shrink-0">
-            <button>
+    <div className="flex gap-10 lg:gap-20 
+    justify-between">
+        <div className="flex gap-4 items-center 
+        flex-shrink-0">
+            <Button variant="ghost" size="icon" className="m-10">
                 <Menu />
-            </button>
+            </Button>
             <a href="/">
                 <img src={logo} className="h-6" />
             </a>
