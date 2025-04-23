87e7698e10ee1c21635707974a09749b67c017d7
Kris <kris.kajtna@gmail.com>
2025-04-23 22:07:53 +0200

    feat: add navigation buttons to CategoryPills component and improve layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,10 @@ export default function App() {
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
         <div>sidebar</div>
+        <div className="overflow-x-hidden px-8 pb4">
         <div className="sticky top-0 bg-white z-10 pb-4">
           <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory}/>
+        </div>
         </div>
       </div>
     </div>
--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,4 +1,6 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "./Button";
+import { useState } from "react";
 
 type CategoryPillProps = {
     categories: string[]
@@ -7,7 +9,11 @@ type CategoryPillProps = {
 }
 
 export function CategoryPills({categories, selectedCategory, onSelect}: CategoryPillProps) {
+    const [isLeftVisible, setIsLeftVisible] = useState(false);
+    const [isRightVisible, setIsRightVisible] = useState(false);
     return (
+    
+
         <div className="overflow-x-hidden relative">
             <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
                 {categories.map((category) => (
@@ -20,6 +26,22 @@ export function CategoryPills({categories, selectedCategory, onSelect}: Category
                 ))}
                 
             </div>
+            {isLeftVisible &&(
+            <div className="absolute left-0 top-1/2 -translate-y-1/2 
+            bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+                <Button variant="ghost" size= "icon" className="h-full aspect-square w-auto p-1.5">
+                    <ChevronLeft />
+                </Button>
+            </div>
+            )}
+            {isRightVisible &&(
+            <div className="absolute right-0 top-1/2 -translate-y-1/2 
+            bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
+                <Button variant="ghost" size= "icon" className="h-full aspect-square w-auto p-1.5">
+                    <ChevronRight />
+                </Button>
+            </div>
+            )}
         </div>
     )
 }
