030a8258b47f27dc4d8ad79f897af769fb66d69c
Kris <kris.kajtna@gmail.com>
2025-04-23 19:35:16 +0200

    feat: add lucide-react dependency and update PageHeader component with new logo

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,3 +1,19 @@
+import { Menu } from 'lucide-react';
+import logo from '../assets/logo3.png';
+
 export function PageHeader() {
-  return null
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between">
+        <div className="flex gap-4 items-center flex-shrink-0">
+            <button>
+                <Menu />
+            </button>
+            <a href="/">
+                <img src={logo} className="h-6" />
+            </a>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
 }
