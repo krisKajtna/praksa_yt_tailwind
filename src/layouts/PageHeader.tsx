a10fd35e634667d7299bf9a72103d4cf2b821a43
Kris <kris.kajtna@gmail.com>
2025-04-23 20:38:21 +0200

    feat: enhance Button component with tailwind-merge and update PageHeader layout with new icons and search functionality

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,22 +1,42 @@
-import { Menu } from 'lucide-react';
+import { Bell, Menu, Mic, Search, Upload, User } from 'lucide-react';
 import logo from '../assets/logo3.png';
 import { Button } from '../components/Button';
 
 export function PageHeader() {
   return (
-    <div className="flex gap-10 lg:gap-20 
-    justify-between">
-        <div className="flex gap-4 items-center 
-        flex-shrink-0">
-            <Button variant="ghost" size="icon" className="m-10">
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
+        <div className="flex gap-4 items-center flex-shrink-0">
+            <Button variant="ghost" size="icon" >
                 <Menu />
             </Button>
             <a href="/">
                 <img src={logo} className="h-6" />
             </a>
         </div>
-        <div></div>
-        <div></div>
+        <form className="flex gap-4 flex-grow justify-center">
+            <div className="flex flex-grow max-w-[600px]">
+                <input type='search' placeholder='Search' 
+                className="rounded-1-full border border-secondary-border shadow-inner 
+                shadow-secondary"/>
+                <Button>
+                    <Search />
+                </Button>
+            </div>
+            <Button type='button' size="icon" className='flex-shrink-0'>
+                <Mic />
+            </Button>
+        </form>
+        <div className="flex flex-shrink-0 md:gap-2 ">
+            <Button variant="ghost" size="icon">
+                <Upload />
+            </Button>
+            <Button variant="ghost" size="icon">
+                <Bell />
+            </Button>
+            <Button variant="ghost" size="icon">
+                <User />
+            </Button>
+        </div>
     </div>
   )
 }
