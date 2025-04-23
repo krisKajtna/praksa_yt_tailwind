e5b53047a07cd488a46f799d819bf626b7c9e534
Kris <kris.kajtna@gmail.com>
2025-04-23 21:51:27 +0200

    feat: implement CategoryPills component with button selection and update App layout

---
+++ src/components/CategoryPills.tsx
@@ -0,0 +1,25 @@
+import { Button } from "./Button";
+
+type CategoryPillProps = {
+    categories: string[]
+    selectedCategory: string
+    onSelect: (category: string) => void
+}
+
+export function CategoryPills({categories, selectedCategory, onSelect}: CategoryPillProps) {
+    return (
+        <div className="overflow-x-hidden relative">
+            <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+                {categories.map((category) => (
+                    <Button key={category} 
+                    onClick={() => onSelect(category)}
+                    variant={selectedCategory === category ? 
+                        "dark" :"default"} className="py-1 px-3 rounded-lg whitespace-nowrap">
+                        {category}
+                    </Button>
+                ))}
+                
+            </div>
+        </div>
+    )
+}
