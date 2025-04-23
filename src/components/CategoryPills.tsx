81ced0ef809fab69f01dd146ddf99bf147b314e9
Kris <kris.kajtna@gmail.com>
2025-04-23 22:22:55 +0200

    feat: implement horizontal scrolling for CategoryPills with button controls

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,6 +1,6 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "./Button";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 type CategoryPillProps = {
     categories: string[]
@@ -8,14 +8,21 @@ type CategoryPillProps = {
     onSelect: (category: string) => void
 }
 
+const TRANSLATE_AMOUNT = 200
+
 export function CategoryPills({categories, selectedCategory, onSelect}: CategoryPillProps) {
+    const [translate, setTranslate] = useState(0);
     const [isLeftVisible, setIsLeftVisible] = useState(false);
     const [isRightVisible, setIsRightVisible] = useState(false);
+    const containerRef = useRef<HTMLDivElement>(null);
     return (
     
 
         <div className="overflow-x-hidden relative">
-            <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+            <div 
+            ref={containerRef}
+            
+            className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]" style={{transform: `translateX(-${translate}px)`}}>
                 {categories.map((category) => (
                     <Button key={category} 
                     onClick={() => onSelect(category)}
@@ -29,7 +36,13 @@ export function CategoryPills({categories, selectedCategory, onSelect}: Category
             {isLeftVisible &&(
             <div className="absolute left-0 top-1/2 -translate-y-1/2 
             bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
-                <Button variant="ghost" size= "icon" className="h-full aspect-square w-auto p-1.5">
+                <Button onClick={() => {
+                    setTranslate(translate => {
+                        const newTranslate = translate - TRANSLATE_AMOUNT;
+                        if (newTranslate <= 0) return 0
+                        return newTranslate
+                    })
+                }} variant="ghost" size= "icon" className="h-full aspect-square w-auto p-1.5">
                     <ChevronLeft />
                 </Button>
             </div>
@@ -37,7 +50,16 @@ export function CategoryPills({categories, selectedCategory, onSelect}: Category
             {isRightVisible &&(
             <div className="absolute right-0 top-1/2 -translate-y-1/2 
             bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
-                <Button variant="ghost" size= "icon" className="h-full aspect-square w-auto p-1.5">
+                <Button 
+                onClick={() => {
+                    if(containerRef.current == null) return
+                    const newTranslate = translate - TRANSLATE_AMOUNT;
+                    const edge = containerRef.current.scrollWidth 
+                    const width = containerRef.current.clientWidth
+                    if (newTranslate + width >= edge) return translate
+                        return newTranslate
+                }}
+                variant="ghost" size= "icon" className="h-full aspect-square w-auto p-1.5">
                     <ChevronRight />
                 </Button>
             </div>
